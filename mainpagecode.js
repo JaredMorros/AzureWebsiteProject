566438492027c3768d7eb0b0a2275344ab27c9b8
JaredMorros <actualdarthvader@gmail.com>
2025-09-08 11:33:55 +0100

    Updated with some basic website contents

--- mainpagecode.js
+++ mainpagecode.js
@@ -1,3 +1,19 @@
+var funky = false
+
 function Saxophone(){
-    
+    const button = document.querySelector('button')
+    const saxophoneaudio = document.getElementById("saxophoneaudio")
+    if (funky == false){
+        document.getElementById("saxaboom").style.display = "block";
+        funky = true;
+        button.innerText = "NO WAIT GO BACK"
+        saxophoneaudio.onload();
+        saxophoneaudio.play();
+    }
+    else{
+        document.getElementById("saxaboom").style.display = "none";
+        funky = false;
+        button.innerText = "Get funky again?";
+        saxophoneaudio.pause();
+    }
 }
